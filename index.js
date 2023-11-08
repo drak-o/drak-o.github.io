7a057930ff8d521fe1dc7b729e4c8e3d367b0fc0
drak-o <105128003+drak-o@users.noreply.github.com>
2023-11-08 10:33:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 window.onload = function() {
     var video = document.getElementById('myVideo');
-    var buttons = document.querySelectorAll('button');
 
     video.addEventListener('loadeddata', function() {
-        // Hide the buttons when the video's metadata is loaded
-        buttons.forEach(function(button) {
-            button.style.display = 'none';
-        });
-    });
-
-    video.addEventListener('canplaythrough', function() {
-        // Show the buttons when the video can play through
-        buttons.forEach(function(button) {
-            button.style.display = 'inline';
-        });
+        video.style.backgroundColor = 'transparent'; // If you want to remove the black background
     });
 }
